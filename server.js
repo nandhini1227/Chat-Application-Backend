47e28dbce4a05a4dc95197d2bb829ffb25c0699b
Nandhini Perumal | Full Stack Developer 🌟 She/Her <150987579+nandhini1227@users.noreply.github.com>
2024-11-09 12:15:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+﻿
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('joinRoom', (room) => {
+        socket.join(room);
+        console.log(`User joined room: ${room}`);
+    });
+
+    socket.on('chatMessage', (msg, room) => {
+        io.to(room).emit('message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
